94c409c0a6fd79b6f59a61437fff8675a8773722
Allison Dennert <allison.dennert@gmail.com>
2020-03-02 13:05:43 -0800

    Committed: 2 March 2020, 1:04pm
    -test commit to check GitHub ops. Edited Line 3.

--- CODE/beetle_script.R
+++ CODE/beetle_script.R
@@ -1,6 +1,6 @@
 ####R Script for Beetle Paper
 ####Originally written in fall 2018 by NR; updated in spring 2020 by NR and AD
-####For submission to Ecology & Evolution
+####For submission to Ecology & Evolution -AD
 
 #### BELLA BELLA BEETLE ANALYSIS 2018 ####
 
